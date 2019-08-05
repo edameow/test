3c3b9cf54b1cbafe742e1c94ce28eb7ea9080a17
edameow <edameow@mail.ru>
2019-08-05 21:03:35 +0300

    patched

--- main.php
+++ main.php
@@ -1,5 +1,4 @@
 <?php
-
 abstract class Animal
 {
     public abstract function getProduct();
@@ -21,17 +20,43 @@ class Cow extends Animal
     }
 }
 
-$countEggs = 0;
-$countMilk = 0;
+class Farm
+{
+    public function products()
+    {
+        $countEggs = $this->getEggs(20);
+        $countMilk = $this->getMilk(10);
 
-for ($i = 1; $i <= 20; $i++) {
-    $chicken = new Chicken();
-    $countEggs += $chicken->getProduct();
+        $data = [
+            'countEggs' => $countEggs,
+            'countMilk' => $countMilk,
+        ];
+        return $data;
+    }
+
+    private function getEggs($countChicken)
+    {
+        $countEggs = 0;
+        for ($i = 1; $i <= $countChicken; $i++) {
+            $chicken = new Chicken();
+            $countEggs += $chicken->getProduct();
+        }
+        return $countEggs;
+    }
+
+    private function getMilk($countCow)
+    {
+        $countMilk = 0;
+        for ($i = 1; $i <= $countCow; $i++) {
+            $cow = new Cow();
+            $countMilk += $cow->getProduct();
+        }
+        return $countMilk;
+    }
 }
 
-for ($i = 1; $i <= 10; $i++) {
-    $cow = new Cow();
-    $countMilk += $cow->getProduct();
-}
+    $farm = new Farm();
+    $data = $farm->products();
 
-echo "Собрано с 20 кур и 10 коров: $countEggs яиц и $countMilk литров молока";
+
+echo "Собрано с 20 кур и 10 коров: {$data['countEggs']} яиц и {$data['countMilk']} литров молока";
